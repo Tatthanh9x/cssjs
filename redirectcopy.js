e946481759e36f1e95d861201659e048236583a3
Tatthanh9x <74942068+Tatthanh9x@users.noreply.github.com>
2020-11-25 21:30:52 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://facebook.com/canhobcons7979'>https://facebook.com/canhobcons7979"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://facebook.com/canhobcons7979'>https://facebook.com/canhobcons7979</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
